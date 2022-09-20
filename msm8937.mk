cdc90711bb23f29e9a5f65c55b54d416b98a8dc8
Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>
2022-09-20 16:43:15 +0530

    msm8937-common: Update perf stack from LA.UM.9.6.2.r1-04200-89xx.0

--- msm8937.mk
+++ msm8937.mk
@@ -278,8 +278,7 @@ endif
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
-    vendor.qti.hardware.perf@2.0 \
-    vendor.qti.hardware.perf@2.0.vendor
+    vendor.qti.hardware.perf@2.2.vendor
 
 # Protobuf
 PRODUCT_PACKAGES += \
